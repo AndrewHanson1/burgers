c5fad524f9953fee526d8efc54a57fd7b2f6c269
unknown <andrew.lewis.hanson@gmail.com>
2019-05-22 21:00:24 -0700

    heroku work please

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const router = express.Router();
 
-// Import the model (cat.js) to use its database functions.
+
 const burger = require("../models/burger.js");
 
 // Create all our routes and set up logic within those routes where required.
@@ -12,7 +12,7 @@ router.get("/", function(req, res) {
       burger: burger
     };
     console.log(handlebarsObject);
-    res.render("main", handlebarsObject);
+    res.render("index", handlebarsObject);
   });
 });
 
